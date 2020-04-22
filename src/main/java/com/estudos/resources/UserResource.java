c43ff570fc8a3a7311203bb516d24fe4cb182fc2
Rodrigo Nascimento <rodrigo.nascimento@castgroup.com.br>
2020-04-22 13:02:35 -0300

    endpoint para retornar os dados de um usuario

--- src/main/java/com/estudos/resources/UserResource.java
+++ src/main/java/com/estudos/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.estudos.resources;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.estudos.domain.Post;
 import com.estudos.domain.User;
 import com.estudos.dto.UserDTO;
 import com.estudos.services.UserService;
@@ -57,4 +60,11 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+
 }
