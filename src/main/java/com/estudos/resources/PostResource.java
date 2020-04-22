f9f6246d2215e5bd2878783b892d6b114ad98a14
Rodrigo Nascimento <rodrigo.nascimento@castgroup.com.br>
2020-04-22 13:39:51 -0300

    consulta simples com querymethods

--- src/main/java/com/estudos/resources/PostResource.java
+++ src/main/java/com/estudos/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.estudos.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudos.domain.Post;
+import com.estudos.resources.util.URL;
 import com.estudos.services.PostService;
 
 @RestController
@@ -23,6 +27,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 
-	 
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text) {
+		text = URL.decodeParam(text);	
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 
 }
--- src/main/java/com/estudos/services/PostService.java
+++ src/main/java/com/estudos/services/PostService.java
@@ -1,5 +1,6 @@
 package com.estudos.services;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
- 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
