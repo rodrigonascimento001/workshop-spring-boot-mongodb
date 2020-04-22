3309f102bc5ad17e580857c638228bffee0ba483
Rodrigo Nascimento <rodrigo.nascimento@castgroup.com.br>
2020-04-22 11:37:20 -0300

    delecao de usuario cm delete

--- src/main/java/com/estudos/services/UserService.java
+++ src/main/java/com/estudos/services/UserService.java
@@ -2,10 +2,8 @@ package com.estudos.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.estudos.domain.User;
 import com.estudos.dto.UserDTO;
 import com.estudos.repository.UserRepository;
@@ -28,6 +26,10 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO(UserDTO objDTO) {
